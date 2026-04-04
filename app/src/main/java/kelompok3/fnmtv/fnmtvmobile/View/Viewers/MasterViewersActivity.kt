099c7950e3366cf8694f6c33e7d3c1c310570a53
KrisnaKintaro <krisnakintaro1256@gmail.com>
2026-04-04 18:08:25 +0700

    Initial commit: Setup struktur folder MVC ala Laravel Lite

---
+++ app/src/main/java/kelompok3/fnmtv/fnmtvmobile/View/Viewers/MasterViewersActivity.kt
@@ -0,0 +1,21 @@
+package kelompok3.fnmtv.fnmtvmobile.View.Viewers
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kelompok3.fnmtv.fnmtvmobile.R
+
+class MasterViewersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_master_viewers)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
