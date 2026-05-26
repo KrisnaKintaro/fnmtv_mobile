8c738ec6fc7451c655b48bca2d3339fd5f6eb8fc
KrisnaKintaro <krisnakintaro1256@gmail.com>
2026-05-26 14:28:11 +0700

    Penambahan comment pada api servis

--- app/src/main/java/kelompok3/fnmtv/fnmtvmobile/data/api/ApiService.kt
+++ app/src/main/java/kelompok3/fnmtv/fnmtvmobile/data/api/ApiService.kt
@@ -19,15 +19,20 @@ import retrofit2.http.Path
 
 
 interface ApiService {
+    // Api get token csrf
+    @GET("../sanctum/csrf-cookie")
+    suspend fun getCsrfCookie(): Response<Unit>
+
+    // Servis untuk memaksa server laravel ngirim data dengan format JSON supaya Android tidak crash kalau server ngirim error HTML
     @Headers("Accept: application/json")
+
+    // Api viewer (home dan master)
     @GET("viewers/berita")
     suspend fun getSemuaBerita(): Response<BeritaResponse>
-
     @GET("viewers/kategori")
     suspend fun getKategori(): Response<KategoriResponse>
 
-    @GET("../sanctum/csrf-cookie")
-    suspend fun getCsrfCookie(): Response<Unit>
+    // Api auth
     @POST("auth/login")
     suspend fun loginUser(
         @Body request: LoginRequest // @Body artinya data dikirim sebagai raw JSON di body request
@@ -36,15 +41,13 @@ interface ApiService {
     @POST("auth/logout")
     suspend fun logoutUser(): Response<Unit>
 
+    // Api manajemen user
     @GET("admin/manajemen_user/ambilData")
     suspend fun getAllUsers(): Response<UserResponse>
-
     @POST("admin/manajemen_user/tambahData")
     suspend fun addUser(@Body user: User): Response<Unit>
-
     @PUT("admin/manajemen_user/ubahData/{id}")
     suspend fun updateUser(@Path("id") id: Int, @Body user: User): Response<Unit>
-
     @DELETE("admin/manajemen_user/hapusData/{id}")
     suspend fun deleteUser(@Path("id") id: Int): Response<Unit>
 }
