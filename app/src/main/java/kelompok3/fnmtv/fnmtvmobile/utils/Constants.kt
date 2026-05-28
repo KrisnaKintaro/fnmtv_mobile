825ca7bd8b4d738cffd8ea434a0eaab6bbfdbe09
lintangmaharani <lintangnasywa23@gmail.com>
2026-05-28 20:50:08 +0700

    feat: sinkronisasi web dengan mobile

--- app/src/main/java/kelompok3/fnmtv/fnmtvmobile/utils/Constants.kt
+++ app/src/main/java/kelompok3/fnmtv/fnmtvmobile/utils/Constants.kt
@@ -2,4 +2,6 @@ package kelompok3.fnmtv.fnmtvmobile.utils
 
 object Constants {
     const val BASE_URL = "https://baru.fenomenatv.com/api/"
+    const val PREF_NAME  = "fnmtv_session"
+    const val KEY_TOKEN  = "token"
 }
