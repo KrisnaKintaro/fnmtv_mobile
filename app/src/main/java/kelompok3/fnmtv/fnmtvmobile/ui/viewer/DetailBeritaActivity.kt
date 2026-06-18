d64e2cf9348678ac102f6a192c2cbfb6bb2055cb
lintangmaharani <lintangnasywa23@gmail.com>
2026-06-18 15:05:05 +0700

    perbaikan 1

--- app/src/main/java/kelompok3/fnmtv/fnmtvmobile/ui/viewer/DetailBeritaActivity.kt
+++ app/src/main/java/kelompok3/fnmtv/fnmtvmobile/ui/viewer/DetailBeritaActivity.kt
@@ -52,6 +52,16 @@ class DetailBeritaActivity : AppCompatActivity() {
             finish()
         }
     }
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+        return when (item.itemId) {
+            // android.R.id.home adalah ID default untuk tombol panah kembali (ic_back) di Toolbar
+            android.R.id.home -> {
+                finish() // Menutup activity detail dan kembali ke halaman sebelumnya
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     private fun setupRecyclerViewKomentar() {
         komentarAdapter = KomentarAdapter()
